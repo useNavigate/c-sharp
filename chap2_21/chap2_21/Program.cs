a9fa030434d6e34ee62456dce472c1604bc0dbe8
useNavigate <useNavigate@gmail.com>
2024-02-01 12:01:04 -0500

    foundation chapter2

---
+++ chap2_21/chap2_21/Program.cs
@@ -0,0 +1,14 @@
+﻿//20-how to use debugger 
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+
+
+//boolean 
+bool isUserInputAbc = userChoice == "ABC";
+
+Console.ReadKey();
