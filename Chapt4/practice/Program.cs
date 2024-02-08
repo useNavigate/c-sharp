6e4139cd08de366c0a005860b69b49e69e8b3177
useNavigate <useNavigate@gmail.com>
2024-02-07 20:35:58 -0500

    123.Json

--- Chapt4/practice/Program.cs
+++ Chapt4/practice/Program.cs
@@ -1,18 +1,33 @@
-﻿
-var sara = new Person("sara");
-Console.WriteLine(sara.Name);
-//sara.Name = "suzu";
-//Console.WriteLine(sara.Name);
+﻿using System.Text.Json;
+
+
+
+// you need a constructor to initiate like this. 
+//var person = new Person("Sara", "Ryu", 1993);
+
+//you do not need constructor to initialize instance like this. 
+var person = new Person { FirstName = "Sara", LastName = "Ryu", YearOfBirth = 1993 };
+
+var asJson = JsonSerializer.Serialize(person);// returns object as JSON string 
+//{"FirstName":"Sara","LastName":"Ryu","YearOfBirth":1993}
+
+
+Console.WriteLine("As Json: ");
+Console.WriteLine(asJson);
+
+var personJson = "{\"FirstName\":\"Sara\",\"LastName\":\"Ryu\",\"YearOfBirth\":1993}";
+var personFromJson = JsonSerializer.Deserialize<Person>(personJson);
 Console.ReadKey();
+public class Person {
+    //public Person(string firstName, string lastName, int yearOfBirth)
+    //{
+    //    FirstName = firstName;
+    //    LastName = lastName;
+    //    YearOfBirth = yearOfBirth;
+    //}
 
-public class Person
-{
-    public string Name { get; private set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int YearOfBirth { get; set; }
 
-    public Person(string name)
-    {
-        Name = name;
-    }
 }
-
-
