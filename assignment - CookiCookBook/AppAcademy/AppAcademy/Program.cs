12cd17bf69727750bf583bd9cbf85451eb5de65a
useNavigate <useNavigate@gmail.com>
2024-02-17 16:47:59 -0500

    jumping to calculator_assignment

--- assignment - CookiCookBook/AppAcademy/AppAcademy/Program.cs
+++ assignment - CookiCookBook/AppAcademy/AppAcademy/Program.cs
@@ -38,16 +38,54 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Animals.Canine dog = new Animals.Canine();
-            Teeth.Canine tooth = new Teeth.Canine();
-            Console.WriteLine("hi");
-            Console.WriteLine(dog);
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            
-            Console.WriteLine(tooth);
-            var x = 1;
-            Console.WriteLine(++x);
-            float money = 0.0000008f;
+            //Animals.Canine dog = new Animals.Canine();
+            //Teeth.Canine tooth = new Teeth.Canine();
+            //Console.WriteLine("hi");
+            //Console.WriteLine(dog);
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+
+            //Console.WriteLine(tooth);
+            //var x = 1;
+            //Console.WriteLine(++x);
+            //float money = 0.0000008f;
+
+            string name;
+
+            Console.Write("Enter Your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine($"hello {name}");
+
+     
+
+            ConsoleKeyInfo keypress;
+
+                Console.Write("Press the Z key to quit...");
+            do
+            {
+                keypress = Console.ReadKey();
+            } while (keypress.Key != ConsoleKey.Z);
+            Console.WriteLine();
+            Console.WriteLine(ConsoleKey.Z);
+
+
+            //Using Parse to Change String Input to Numbers
+            int a, b;
+            string input;
+
+            Console.Write("Enter a number: ");
+            input = Console.ReadLine();
+
+            a = int.Parse(input);
+
+            Console.Write("Enter another number: ");
+            input = Console.ReadLine();
+
+            b = int.Parse(input);
+
+            // Console.WriteLine("The sum is: " + (a+b).ToString());
+            int sum = a + b;
+            Console.WriteLine("The sum is: " + sum.ToString());
+
 
             Console.ReadLine();
         }
