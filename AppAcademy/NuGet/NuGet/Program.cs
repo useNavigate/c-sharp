ea674146f62e4fdf1012c209c4e4ef292db20992
useNavigate <useNavigate@gmail.com>
2024-02-20 19:37:42 -0500

    nuGet

---
+++ AppAcademy/NuGet/NuGet/Program.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] rolls = { 2, 3, 9, 15, 11, 17, 22, 8, 6, 5, 2 };
+        PrintStatistics(rolls);
+        Console.ReadKey();
+    }
+
+    static void PrintStatistics(int[] rolls)
+    {
+        // Create a new BarChart
+        var chart = new BarChart()
+            .Width(60)
+            .Label("[green bold underline]Roll Statistics[/]")
+            .CenterLabel();
+
+        // Add data points for each roll
+        for (int i = 2; i <= 12; i++)
+        {
+            chart.AddItem(i.ToString(), rolls[i - 2], Color.Yellow);
+        }
+
+        // Write the chart to the console
+        AnsiConsole.Render(chart);
+    }
+}
---
+++ AppAcademy/interface/interface/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+        
+        
+        }
+        static void CallPrint(IPrintable printable)
+        {
+            printable.Print("Message");
+        }
+    }
+
+    public class Fasmaxhine : IPrintable, IReadable
+    {
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public string Read()
+        {
+            return Console.ReadLine();
+        }
+    }
+
+    public interface IReadable
+    {
+        string Read();
+    }
+    public interface IPrintable
+    {
+        void Print(string message);
+    }
+}
