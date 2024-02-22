75543bc45f13d2ab186c3d971a46857f40586e1f
useNavigate <useNavigate@gmail.com>
2024-02-21 20:41:14 -0500

    functions

--- CrashCourse/crashCourse/crashCourse/Program.cs
+++ CrashCourse/crashCourse/crashCourse/Program.cs
@@ -9,121 +9,123 @@ namespace CrashCourse
 {
     public class Program
     {
+        //FUNCTIONS
+        /*
+         <Access Specifier> <Return Type> <Method Name>(Parameters)
+          { <Body> }
+        
+
+        Access Specifier determines weather the function can be called from another class
+        - public : can be accessed from another class 
+        - private : can't be accessed from another class 
+        - protected : can't be accessed by class and derived classes 
+         */
+
+        static double GetSum(double x = 1, double y = 1)
+        {
+           //traditional way to swap
+            //double temp = x;
+            //x = y;
+            //y = temp;
+
+           //new way to swap[ 
+            (x, y) = (y, x);
+            return x + y;
+        }
+
+        static void DoubleIt(int x, out int solution)
+        {
+            solution = x * 2;
+        }
+
+        public static void Swap(ref int num3, ref int num4)
+        {
+            int temp = num3;
+            num3 = num4;
+            num4 = temp; 
+        }
+
+        static double GetSumMore(params double[] nums)
+        {
+            double sum = 0;
+            foreach (var i in nums)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        static void PrintInfo(string name, int zipCode)
+        {
+            Console.WriteLine("{0} lives in the zip code {1}", name, zipCode);
+        }
+
+        //METHOD OVERLOADING
+        static double GetSum2(double x = 1, double y = 1)
+        {
+            return x + y;
+        }
+
+        static double GetSum2(string x = "1", string y = "1")
+        {
+            double dblX = Convert.ToDouble(x);
+            double dblY = Convert.ToDouble(y);
+
+            return dblX + dblY;
+        }
+
+
         static void Main(string[] args) {
+            //GetSum
+            double x = 6;
+            double y = 4;
+            Console.WriteLine("6 + 4 = " + GetSum(x,y));//10
 
-            StringBuilder ssb = new StringBuilder("Same");
-            StringBuilder ssb1 = new StringBuilder("Same");
+            //DoubleIt
+            int solution;
+            DoubleIt(15, out solution);
+            Console.WriteLine("15 * 2  = " + solution); //30
 
-            Console.WriteLine(ssb.Equals(ssb1)); //True because it has same string 
-            Console.WriteLine(ssb == ssb1); //False because memory location are different. 
+            //REF / swapping
+            int num3 = 10;
+            int num4 = 20;
+            Console.WriteLine("Before Swap num1 : {0},{1}",num3,num4);//10,20
 
-
-
-
-            StringBuilder sb = new StringBuilder("Random text");
-            StringBuilder sb2 = new StringBuilder("More stuff that is very important",256);
-
-            Console.WriteLine("Capacity : {0}",sb2.Capacity); //256
-            Console.WriteLine("Capacity : {0}", sb.Capacity); //16
-            Console.WriteLine(sb2.Length); //33 thre are exactly 33 chars in sb2
-            sb2.AppendLine("\nMore important text"); // adding a line 
 
             /*
-             StringBuilder automatically increases its capacity to accommodate 
-             larger strings when necessary. If the initial capacity provided is insufficient, 
-             StringBuilder dynamically reallocates memory to increase its capacity.
-             
-            
-             example code 👇
+             Passing by reference: Allows a method to modify the original variable directly rather than a copy.
+             Performance considerations: Can improve performance by avoiding unnecessary copying of large data structures.
+             Usage: Both caller and callee must use ref keyword; parameters must be initialized before passing.
+             */
+            Swap(ref num3, ref num4);
+            Console.WriteLine("After Swap num1 : {0},{1}", num3, num4); //20,10 its mutated 
+
+
+
+            //GetSumMore
+            short num11 = 1;
+            short num12 = 2;
+            short num13 = 3;
+
+            Console.WriteLine("1+2+3 ={0}", GetSumMore(num11,num12,num13));
+            /*
+             as you see we never parse it or converted 
+            this is c# magic if same type is implicit convertable it will just do it 
+            so as you see eventhough num11 num12 num13 are short type because our method 
+            GetSumMore supposed to take double value, C#'s magical implicit numeric converesion happend here
+            and thats why its not erroing. 
              */
 
-            Console.WriteLine("---------------------------------");
-            StringBuilder sb3 = new StringBuilder("definately has more than 1 char", 1);
-
-            Console.WriteLine("Capacity : {0}", sb3.Capacity); //31
-            Console.WriteLine(sb3.Length); //31 sb3
-            sb3.AppendLine();//adding an empty line \n
-            sb3.AppendLine("Adding More Line "); // appendingn text
-            sb3.AppendLine("\nanother line with \\n ");
-            Console.WriteLine("Capacity : {0}", sb3.Capacity); //124
-
-            Console.WriteLine("sb3 : \n" + sb3);
-
-            
-
-            Console.WriteLine("-----------------| Culture specific format");
-            //culture specific format 
-            CultureInfo enUs = CultureInfo.CreateSpecificCulture("en-US");
-            string bestCust = "Bob Smith";
-            sb2.AppendFormat(enUs, "Best Customer : {0}", bestCust);
-            Console.WriteLine(sb2.ToString()); // same as  Console.WriteLine(sb2);
-            Console.WriteLine("==================================");
 
 
-            //replace
-            Console.WriteLine("-----------------| Replace");
-            sb2.Replace("text", "REPLACED");
-
-            Console.ForegroundColor= ConsoleColor.Green;
-            Console.WriteLine("after replace :" + sb2.ToString());
-            Console.ForegroundColor = ConsoleColor.White;
-
-            Console.WriteLine("~_~_~_~_~~_~_~_~~_~_~_~_~_");
-            sb2.Clear();//clearing
-            Console.WriteLine(sb2); //nothing. literally prints nothing
-            Console.WriteLine(sb2.Capacity); //256 capacity still remains the same. 
-           
-            
-            Console.WriteLine("-----------------| appending && inserting text");
-
-            //appending / inserting text
-            sb2.Append(" Random Text\n");
-            sb2.Insert(0, "that's great");//StringBuilder.Insert(idx,string value)
-                                          //now sb2 =>  that's great Random Text
-
-            //checking if those two stringbuilder are equal 
-            Console.WriteLine(sb.Equals(sb2)); //False 
-
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine(sb2); //that's great Random Text
-  
-
-            //Removing
-
-            sb2.Remove(0, 7);// removing "that's " 7 chars including space after s 
-            Console.WriteLine(sb2.ToString()); //great Random Text
-            Console.ForegroundColor = ConsoleColor.White;
+            //PrintInfo
+            //this code demonstrates how to pass down arguments into parameter without following its order 
+            PrintInfo(zipCode: 11324, name:"Faker Park");
 
 
-
-
-
-
-            Console.WriteLine("\n------------|  en-US");
-            // Format a number (currency) using enUs culture
-            double price = 1234.56;
-            string formattedPrice = price.ToString("C", enUs); // Formatted price: $1,234.56
-            Console.WriteLine("Formatted price: " + formattedPrice);
-
-            // Format a date using enUs culture
-            DateTime date = DateTime.Now;
-            string formattedDate = date.ToString("D", enUs); // Formatted date: Wednesday, February 21, 2024
-            Console.WriteLine("Formatted date: " + formattedDate);
-
-
-            Console.WriteLine();
-            Console.WriteLine("\n------------|  ko-KOR");//Formatted price: ?1,235
-            CultureInfo koKr = CultureInfo.CreateSpecificCulture("ko-KOR");//Formatted date: 2024? 2? 21? ???
-            double pr = 1234.56;
-            string fPrice = pr.ToString("C", koKr); // Currency format
-            Console.WriteLine("Formatted price: " + fPrice);
-
-            DateTime dt = DateTime.Now;
-            string fDate = dt.ToString("D", koKr); // Long date format
-            Console.WriteLine("Formatted date: " + fDate);
-
-
-
+            //Method overload 
+            Console.WriteLine("5.0 + 4.0 = {0}",GetSum2(5.0,4.0));
+            Console.WriteLine("5.0 + 4.0 = {0}", GetSum2("5", "4"));
             Console.ReadKey();
         }
     }
