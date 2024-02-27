0dcf46698cff7ec7f7499fda0ea62d413d9fb05d
useNavigate <useNavigate@gmail.com>
2024-02-27 12:32:51 -0500

    Directory Functions

--- CrashCourse/crashCourse/FI/Program.cs
+++ CrashCourse/crashCourse/FI/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace fileSystem
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo currDir = new DirectoryInfo("."); // . for current directory 
+            DirectoryInfo saraDir = new DirectoryInfo(@"C:\Users\UseNa");
+            Console.WriteLine(saraDir.FullName);
+            Console.WriteLine(saraDir.Name);
+            Console.WriteLine(saraDir.Parent);
+            Console.WriteLine(saraDir.Attributes);
+            Console.WriteLine(saraDir.CreationTime);
+
+            DirectoryInfo dataDir = new DirectoryInfo(@"C:\Users\UseNa\testing");
+            Console.WriteLine(dataDir.CreationTime);// you can change the dir name but as you see this one is just created
+            dataDir.Create();
+            //deleting dir 
+
+            Directory.Delete(@"C:\Users\UseNa\testing");
+            Console.ReadKey();
+
+        }
+
+    }
+}
