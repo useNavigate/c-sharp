b7edf763e4c7201c16d74cf804d203005ed467ce
useNavigate <useNavigate@gmail.com>
2024-02-27 15:14:21 -0500

    BinaryReadder and BinaryWriter

--- CrashCourse/crashCourse/BinaryWriterAndReader/Program.cs
+++ CrashCourse/crashCourse/BinaryWriterAndReader/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BinaryWriterAndReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Writing binary data 
+            //* creating file
+            string textFilePath4 = @"C:\Users\UseNa\csharp_fi\textfile4.txt";
+            FileInfo datFile = new FileInfo(textFilePath4);
+
+            //* Writing file
+            BinaryWriter bw = new BinaryWriter(datFile.OpenWrite());
+            string randText = "Random Text";
+            int myAge = 47;
+            double height = 6.25;
+            bw.Write(randText);
+            bw.Write(myAge);
+            bw.Write(height);
+            bw.Close();
+
+
+            //Reading binary data
+            BinaryReader br = new BinaryReader(datFile.OpenRead());
+            Console.WriteLine(br.ReadString());
+            Console.WriteLine(br.ReadInt32());
+            Console.WriteLine(br.ReadDouble());
+            br.Close();
+            Console.ReadKey();
+        }
+    
+    }
+
+}
