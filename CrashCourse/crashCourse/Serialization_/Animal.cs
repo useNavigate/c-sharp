11b8e5ea882b9403a562e47789d8a0d617612cf6
useNavigate <useNavigate@gmail.com>
2024-02-27 17:00:34 -0500

    JsonSerializer

---
+++ CrashCourse/crashCourse/Serialization_/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_
+{
+    [Serializable()]
+    public class Animal : ISerializable
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int AnimalID { get; set; }
+
+        public Animal() { }
+
+        public Animal(string name = "No Name", double weight = 0, double height = 0,int animalID=0)
+        {
+            Name = name;
+            Weight = weight;
+            Height = height;
+            AnimalID = animalID;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name : {0}\t Weight: {1}\t Height:{2}\t ID:{3}", Name, Weight, Height,AnimalID);
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Weight", Weight);
+            info.AddValue("Height", Height);
+            info.AddValue("AnimalID", AnimalID);
+        }
+
+        public Animal(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Weight = (double)info.GetValue("Weight", typeof(double));
+            Height = (double)info.GetValue("Height", typeof(double));
+            AnimalID = (int)info.GetValue("AnimalID", typeof(int));
+
+
+
+        }
+    }
+}
---
+++ CrashCourse/crashCourse/Serialization_/Program.cs
@@ -0,0 +1,65 @@
+﻿//using System.IO;
+//using System.Runtime.Serialization.Formatters.Binary;
+//using System.Xml.Serialization;
+
+
+using Serialization_;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+namespace serialization_
+{
+    public class Program
+    {
+        /*
+         serialization able to do is actually store the state of an objejct 
+        in a file stream and you can pass it to a remot network or w.e
+         */
+
+
+        // code below is old way from crash course video so i commented out 
+        static void Main(string[] args)
+        {
+
+
+            Animal bowser = new Animal("Bowser", 45, 25,1);
+            ////now serialize data to a file 
+            //Stream stream = File.Open("AnimalData.dat", FileMode.Create);
+            ////thisway wont work anymore
+            //BinaryFormatter bf= new();
+            //bf.Serialize(stream, bowser);
+            //stream.Close();
+
+            //bowser = null;
+
+            //stream = File.Open("AnimalData.dat", FileMode.Open);
+            //bf = new BinaryFormatter();
+
+            //bowser =(Animal)bf.Deserialize(stream);
+            //stream.Close();
+
+            //Console.WriteLine(bowser.ToString());
+
+            //--------------------------------------------| JSON Serialization
+
+            // Serialize object to JSON and write it to a file
+            string filePath = @"C:\Users\UseNa\csharp_fi\AnimalData.dat" ;
+            string jsonData = JsonSerializer.Serialize(bowser);
+
+            File.WriteAllText(filePath, jsonData);
+
+            bowser = null;
+
+            // Read JSON data from file and deserialize it back to an object
+           
+            string jsonString = File.ReadAllText(filePath);//열어보면 제이슨 파일처럼 생겼음
+            bowser = JsonSerializer.Deserialize<Animal>(jsonString);
+
+            Console.WriteLine(bowser.ToString());
+            Console.ReadKey();
+
+        
+    
+        }
+    }
+}
