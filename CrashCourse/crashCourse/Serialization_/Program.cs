bcbcd2bb2bb8b310cb801df838068bd97da5cb4f
useNavigate <useNavigate@gmail.com>
2024-02-27 18:08:25 -0500

    xml serializing and deserializing

--- CrashCourse/crashCourse/Serialization_/Program.cs
+++ CrashCourse/crashCourse/Serialization_/Program.cs
@@ -6,6 +6,7 @@
 using Serialization_;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace serialization_
 {
@@ -43,23 +44,83 @@ namespace serialization_
             //--------------------------------------------| JSON Serialization
 
             // Serialize object to JSON and write it to a file
-            string filePath = @"C:\Users\UseNa\csharp_fi\AnimalData.dat" ;
+            string jsonFilePath = @"C:\Users\UseNa\csharp_fi\jsonSerializer.json" ;
             string jsonData = JsonSerializer.Serialize(bowser);
 
-            File.WriteAllText(filePath, jsonData);
+            //Using statement ensures the StreamWriter is properly disposed 
+            using (StreamWriter jsonWriter = new StreamWriter(jsonFilePath))
+            { 
+                jsonWriter.Write(jsonData);
+            }
 
-            bowser = null;
+            //Read JSON data from file and deserialize it back to an object 
+            string jsonContent;
+            using (StreamReader jsonReader = new StreamReader(jsonFilePath))
+            {
+                jsonContent = jsonReader.ReadToEnd();
+            }
+            // After deserialization and when you're done with the file
+           // File.Delete(filePath);
 
-            // Read JSON data from file and deserialize it back to an object
-           
-            string jsonString = File.ReadAllText(filePath);//열어보면 제이슨 파일처럼 생겼음
-            bowser = JsonSerializer.Deserialize<Animal>(jsonString);
 
             Console.WriteLine(bowser.ToString());
-            Console.ReadKey();
 
-        
-    
+            bowser.Weight = 99999;
+            //--------------------------------------------| XML Serialization
+
+ 
+
+            //1. make the file path 
+            string xmlfilePath = @"C:\Users\UseNa\csharp_fi\xmalSerialization.xml";
+            //2. create the serializer 
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Animal));
+            //3. while using streamWriter serialize the data => write the file
+            using (TextWriter textWriter = new StreamWriter(xmlfilePath))
+            {
+                xmlSerializer.Serialize(textWriter, bowser);
+            }
+
+            bowser = null;
+            // Read XML data from file and deserialize it back to an object
+            using (TextReader reader = new StreamReader(xmlfilePath))
+            {
+                bowser = (Animal)xmlSerializer.Deserialize(reader);
+            }
+
+            // Delete XML file
+           // File.Delete(xmlfilePath);
+
+            Console.WriteLine(bowser.ToString());
+            List<Animal> theAnimals = new List<Animal>
+            {
+                new Animal("Mario",60,30,2),
+                new Animal("Luigi",55,24,3),
+                new Animal("Peach",40,20,4)
+
+            };
+
+            string animalFL = @"C:\Users\UseNa\csharp_fi\animalSerialization.xml";
+            using (Stream fs = new FileStream(animalFL, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer2 = new XmlSerializer(typeof(List<Animal>));
+                serializer2.Serialize(fs, theAnimals);
+            }
+            theAnimals = null;
+
+
+            //open and read file 
+            XmlSerializer serializer3 = new XmlSerializer(typeof(List<Animal>));
+            using (FileStream fs2 = File.OpenRead(@"C:\Users\UseNa\csharp_fi\animalSerialization.xml")) 
+            { 
+                theAnimals = (List<Animal>)serializer3.Deserialize(fs2);
+            }
+            foreach (Animal a in theAnimals)
+            {
+                Console.WriteLine(a.ToString());
+            }
+                Console.ReadKey();
+
+
         }
     }
 }
