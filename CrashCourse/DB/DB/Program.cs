10df36c97c989391ada79586fca5261900fe6ce1
useNavigate <useNavigate@gmail.com>
2024-02-27 20:11:18 -0500

    connected to MS SQL starting WPF APP

---
+++ CrashCourse/DB/DB/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+ 
+ 1. goto view 
+ 2. click server explorer 
+ 3. right click data
+ 4. click add connection 
+ 5. make sure to check the box where it says trust this person w.e
+ 6. file new project  => wpf app (.Net framework)
+
+ */
